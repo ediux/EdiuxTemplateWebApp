02f94eb1da7475f7ffcad982b7f4196e287395ae
ediux <ediux@msn.com>
2016-11-29 18:04:51 +0800

    加入專案檔案。

---
+++ EdiuxTemplateWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EdiuxTemplateWebApp.Startup))]
+namespace EdiuxTemplateWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

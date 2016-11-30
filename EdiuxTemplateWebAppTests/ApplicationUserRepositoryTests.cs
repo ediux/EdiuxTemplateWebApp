fb84220bf2511686d7a169d7b851bedcb537f01e
ediux <ediux@msn.com>
2016-11-30 19:02:20 +0800

    ApplicationUserRepository的測試程式碼

---
+++ EdiuxTemplateWebAppTests/ApplicationUserRepositoryTests.cs
@@ -0,0 +1,240 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EdiuxTemplateWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdiuxTemplateWebApp.Models.Tests
+{
+    [TestClass()]
+    public class ApplicationUserRepositoryTests
+    {
+        private IApplicationUserRepository testUserRepo;
+        private ApplicationUser _user;
+#if CS6
+        private const string KeyName = nameof(ApplicationUser);
+#else
+        private static string KeyName = typeof(ApplicationUser).Name;
+#endif
+        [TestInitialize]
+        public void init()
+        {
+            testUserRepo = RepositoryHelper.GetApplicationUserRepository();
+
+        }
+
+        [TestCleanup]
+        public void GC()
+        {
+            if (testUserRepo.Where(w => w.UserName == "TestUser").Any())
+            {
+                ApplicationUser user = testUserRepo.All().FirstOrDefault(w => w.UserName == "TestUser");
+
+                if (user != null)
+                {
+                    testUserRepo.Delete(user);
+                    testUserRepo.UnitOfWork.Commit();
+                }
+            }
+
+            testUserRepo.Dispose();
+        }
+
+        [TestMethod()]
+        public void AllTest()
+        {
+            try
+            {
+                var result = testUserRepo.All();
+                Assert.IsNotNull(result);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+
+
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            try
+            {
+                _user = ApplicationUser.Create();
+
+                _user.UserName = "TestUser";
+                _user.Password = "!QAZ2wsx";
+                _user.DisplayName = "TestUser";
+                _user.Void = false;
+
+                var result = testUserRepo.Add(_user);
+
+                testUserRepo.UnitOfWork.Commit();
+                result = testUserRepo.Reload(result);
+                Assert.AreNotSame(_user.Id, result.Id);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            try
+            {
+                FindByNameAsyncTest();
+
+                testUserRepo.Delete(_user);
+                testUserRepo.UnitOfWork.Commit();
+
+                var isDeleted = testUserRepo.Get(_user.Id);
+                Assert.IsNotNull(isDeleted);
+                Assert.AreEqual(true, isDeleted.Void);
+
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+
+
+        }
+
+        [TestMethod()]
+        public void ClearCacheTest()
+        {
+            try
+            {
+                testUserRepo.ClearCache(KeyName);
+                Assert.IsFalse(testUserRepo.UnitOfWork.IsSet(KeyName));
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod()]
+        public void CreateAsyncTest()
+        {
+            try
+            {
+                CreateTestUser();
+
+                testUserRepo.CreateAsync(_user);
+                FindByNameAsyncTest();
+
+                Assert.IsNotNull(_user);
+                Assert.AreEqual("TestUser", _user.UserName);
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message == "User 'TestUser' is existed.")
+                    Assert.AreEqual("User 'TestUser' is existed.", ex.Message);
+                else
+                    Assert.Fail(ex.Message);
+            }
+        }
+
+        private void CreateTestUser()
+        {
+            _user = ApplicationUser.Create();
+
+            _user.UserName = "TestUser";
+            _user.Password = "!QAZ2wsx";
+            _user.DisplayName = "TestUser";
+            _user.Void = false;
+        }
+
+        [TestMethod()]
+        public async void DeleteAsyncTest()
+        {
+            try
+            {
+                FindByNameAsyncTest();
+                await testUserRepo.DeleteAsync(_user);
+                FindByNameAsyncTest();
+                Assert.IsNull(_user);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod()]
+        public void FindByIdAsyncTest()
+        {
+            try
+            {
+                if (_user == null)
+                {
+                    _user = testUserRepo.All()
+                        .Where(w => w.UserName != "root").First();
+                }
+
+                Task<ApplicationUser> testinguser = testUserRepo.FindByIdAsync(_user.Id);
+                testinguser.Wait();
+
+                ApplicationUser queryResutl = testinguser.Result;
+                Assert.IsNotNull(queryResutl);
+            }
+            catch (Exception ex)
+            {
+
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod()]
+        public async void FindByNameAsyncTest()
+        {
+            try
+            {
+                _user = await testUserRepo.FindByNameAsync("TestUser");
+                Assert.AreNotEqual(0, _user.Id);
+            }
+            catch (Exception ex)
+            {
+
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod()]
+        public void GetCacheTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetRolesAsyncTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void IsInRoleAsyncTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void RemoveFromRoleAsyncTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void UpdateAsyncTest()
+        {
+
+        }
+    }
+}

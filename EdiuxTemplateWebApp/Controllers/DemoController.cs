e6ae2c88fad7605d89dc3b95dcdac59512068f38
ediux <ediux@msn.com>
2016-12-05 02:38:45 +0800

    範例網站的MVC化

--- EdiuxTemplateWebApp/Controllers/DemoController.cs
+++ EdiuxTemplateWebApp/Controllers/DemoController.cs
@@ -6,10 +6,55 @@ using System.Web.Mvc;
 
 namespace EdiuxTemplateWebApp.Controllers
 {
+    [AllowAnonymous]
     public class DemoController : Controller
     {
         // GET: Demo
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult General()
+        {
+            return View();
+        }
+
+        public ActionResult Buttons()
+        {
+            return View();
+        }
+
+        public ActionResult Tabs()
+        {
+            return View();
+        }
+
+        public ActionResult Accordions()
+        {
+            return View();
+        }
+
+        public ActionResult NestableList()
+        {
+            return View();
+        }
+        public ActionResult Grid()
+        {
+            return View();
+        }
+
+        public ActionResult Dialogs()
+        {
+            return View();
+        }
+
+        public ActionResult FormElements()
+        {
+            return View();
+        }
+
+        public ActionResult FormValidation()
         {
             return View();
         }

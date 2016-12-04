3cd86055ee23e59ff85b3fe4f5435caaafdf10e6
ediux <ediux@msn.com>
2016-12-05 00:21:29 +0800

    加入控制器動作的檢視模型

---
+++ EdiuxTemplateWebApp/Models/ApplicationLevelModels/ControllerActionViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EdiuxTemplateWebApp.Models
+{
+    public class ControllerActionViewModel
+    {
+        [Required]
+        [Display(Name = "資料識別碼")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "動作名稱")]
+        public string Name { get; set; }
+    }
+}

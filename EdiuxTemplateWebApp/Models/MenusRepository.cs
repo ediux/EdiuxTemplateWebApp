14a7fe6160790b062f7a37e91bb2e8672c5c261c
ediux <ediux@msn.com>
2016-12-04 23:28:19 +0800

    複寫All()方法只傳回未被標記為作廢的項目

--- EdiuxTemplateWebApp/Models/MenusRepository.cs
+++ EdiuxTemplateWebApp/Models/MenusRepository.cs
@@ -2,16 +2,30 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace EdiuxTemplateWebApp.Models
-{   
-	public  partial class MenusRepository : EFRepository<Menus>, IMenusRepository
-	{
+{
+    public partial class MenusRepository : EFRepository<Menus>, IMenusRepository
+    {
+        public override IQueryable<Menus> All()
+        {
 
-	}
+            var result = base.All().Include(m => m.ChildMenus).Include(m => m.System_ControllerActions)
+                 .Where(w => w.Void == false)
+                 .AsQueryable();
 
-	public  partial interface IMenusRepository : IRepositoryBase<Menus>
-	{
+            if (result.Count() > 0)
+                return result;
 
-	}
+            result.Load();
+
+            return result;
+        }
+    }
+
+    public partial interface IMenusRepository : IRepositoryBase<Menus>
+    {
+
+    }
 }

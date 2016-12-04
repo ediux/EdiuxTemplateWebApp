aa9f80abb95d79ec2601e90e0eff04ba6acb3221
ediux <ediux@msn.com>
2016-12-05 00:21:51 +0800

    加入共用的基底控制器類別

---
+++ EdiuxTemplateWebApp/Controllers/BaseController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EdiuxTemplateWebApp.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            Elmah.ErrorSignal.FromCurrentContext().Raise(filterContext.Exception);
+
+            if(filterContext.Exception is System.Data.Entity.Validation.DbEntityValidationException)
+            {
+                filterContext.ExceptionHandled = true;
+
+                filterContext.Result = View("DbEntityValidationException", filterContext.Exception);
+   
+                return;
+            }
+
+            //if(filterContext.HttpContext.Response.StatusCode == 404)
+            //{
+
+            //}
+            base.OnException(filterContext);
+        }
+
+        protected override void HandleUnknownAction(string actionName)
+        {
+            base.HandleUnknownAction(actionName);
+        }
+    }
+}

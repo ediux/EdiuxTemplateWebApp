7fdc77c76d3fe17c52473279ff41457b5c1b12cc
ediux <ediux@msn.com>
2016-12-07 02:57:13 +0800

    加入自訂錯誤處理機制

---
+++ EdiuxTemplateWebApp/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EdiuxTemplateWebApp.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index(string actionName,string controllerName)
+        {
+            return View("Error", new HandleErrorInfo(Server.GetLastError(), actionName, controllerName));
+        }
+    }
+}

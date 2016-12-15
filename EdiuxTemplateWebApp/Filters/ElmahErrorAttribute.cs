940f13055822393aaa55b402638adef1da040aed
ediux <ediux@msn.com>
2016-12-15 21:24:00 +0800

    elmah錯誤過濾器

---
+++ EdiuxTemplateWebApp/Filters/ElmahErrorAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Http.Filters;
+using System.Web.Mvc;
+
+namespace EdiuxTemplateWebApp.Filters
+{
+    public class ElmahErrorAttribute : ExceptionFilterAttribute
+    {
+
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            if (actionExecutedContext.Exception != null)
+                Elmah.ErrorSignal.FromCurrentContext().Raise(actionExecutedContext.Exception);
+
+            base.OnException(actionExecutedContext);
+        }
+    }
+}

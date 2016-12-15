1a00dcfdc0bdf96f5680ebe93934c191ba8f40d6
ediux <ediux@msn.com>
2016-12-15 21:40:31 +0800

    移除元件掃描

--- EdiuxTemplateWebApp/Global.asax.cs
+++ EdiuxTemplateWebApp/Global.asax.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using EdiuxTemplateWebApp.Models;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -18,6 +15,8 @@ namespace EdiuxTemplateWebApp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+          
         }
     }
 }

1b6a331fbeb09dbac4b0c9cf5e1d812c1c566443
ediux <ediux@msn.com>
2017-01-20 16:11:06 +0800

    變更設定和新增啟動初始化類別

---
+++ EdiuxTemplateWebApp/App_Start/Startup.DataStore.cs
@@ -0,0 +1,74 @@
+﻿using EdiuxTemplateWebApp.Models.AspNetModels;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EdiuxTemplateWebApp
+{
+    partial class Startup
+    {
+        public void ConfigureDataStore(IAppBuilder app)
+        {
+            //檢查Application是否已經註冊?
+
+            if (checkCurrentAppIsRegistered()==false)
+            {
+                registerApplication(getApplicationNameFromConfiguationFile());
+            }else
+            {
+                addToMemoryCache();
+            }
+            
+        }
+
+        private void addToMemoryCache()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void registerApplication(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool checkCurrentAppIsRegistered()
+        {
+            try
+            {
+                Iaspnet_ApplicationsRepository appRepo = RepositoryHelper.Getaspnet_ApplicationsRepository();
+
+                string appName = getApplicationNameFromConfiguationFile();
+
+                if (appRepo.FindByName(appName) == null)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+         
+        }
+
+        private static string getApplicationNameFromConfiguationFile()
+        {
+            string appName;
+            if (System.Web.Configuration.WebConfigurationManager.AppSettings.AllKeys.Contains("ApplicationName"))
+            {
+                appName = System.Web.Configuration.WebConfigurationManager.AppSettings["ApplicationName"];
+            }
+            else
+            {
+                appName = typeof(MvcApplication).Namespace;
+            }
+
+            return appName;
+        }
+    }
+}

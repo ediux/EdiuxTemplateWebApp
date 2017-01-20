aef681c4196ef9b6fb14e9906e342ab89a025f16
ediux <ediux@msn.com>
2017-01-20 16:40:58 +0800

    移除自動註冊程式碼，自動註冊改在啟動類別裡進行。

--- EdiuxTemplateWebApp/Filters/ApplicationIdentifyAttribute.cs
+++ EdiuxTemplateWebApp/Filters/ApplicationIdentifyAttribute.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.Caching;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace EdiuxTemplateWebApp.Filters
 {
@@ -24,30 +25,16 @@ namespace EdiuxTemplateWebApp.Filters
         {
             aspnet_Applications fromCache = MemoryCache.Default.Get("ApplicatinInfo") as aspnet_Applications;
 
-            string appName = string.Empty;
-
             if (fromCache == null)
             {
-                if (System.Web.Configuration.WebConfigurationManager.AppSettings.AllKeys.Contains("ApplicationName"))
-                {
-                    appName = System.Web.Configuration.WebConfigurationManager.AppSettings["ApplicationName"];
-                }
-                else
-                {
-                    appName = typeof(MvcApplication).Namespace;
-                }
-
-                fromCache = new aspnet_Applications();
-                fromCache.ApplicationId = Guid.NewGuid();
-                fromCache.ApplicationName = appName;
-                fromCache.Description = appName;
-                fromCache.LoweredApplicationName = appName.ToLowerInvariant();
-
-                fromCache = appRepo.Add(fromCache);
-                
-                filterContext.Controller.ViewBag.ApplicationInfo = fromCache;
-
-                MemoryCache.Default.Set("ApplicatinInfo", fromCache, DateTime.Now.AddMinutes(30));
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary
+                    {
+                        { "controller", "Error" },
+                        { "action", "Index" },
+                        { "actionName",filterContext.ActionDescriptor.ActionName},
+                        { "controllerName",filterContext.ActionDescriptor.ControllerDescriptor.ControllerName },
+                    });
+                return;
             }
 
             filterContext.Controller.ViewBag.ApplicationInfo = fromCache;

158513e3866f20fe72739ebead50e499f0eeb4e1
ediux <ediux@msn.com>
2017-01-26 02:14:52 +0800

    使用者儲存庫程式碼增修

--- EdiuxTemplateWebApp/Models/AspNetModels/partailClasses/aspnet_UsersRepository.partialClass.cs
+++ EdiuxTemplateWebApp/Models/AspNetModels/partailClasses/aspnet_UsersRepository.partialClass.cs
@@ -7,6 +7,8 @@ using System.Security.Claims;
 using System.Data.Entity;
 using System.Runtime.Caching;
 using Microsoft.AspNet.Identity;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace EdiuxTemplateWebApp.Models.AspNetModels
 {
@@ -25,13 +27,13 @@ namespace EdiuxTemplateWebApp.Models.AspNetModels
 
                 return InternalDatabaseAlias.aspnet_Users
                     .Where(w => w.ApplicationId == application.ApplicationId)
-                    .Include(p=>p.aspnet_Applications)
-                    .Include(p=>p.aspnet_Membership)
-                    .Include(p=>p.aspnet_PersonalizationPerUser)
-                    .Include(p=>p.aspnet_Profile)
-                    .Include(p=>p.aspnet_Roles)
-                    .Include(p=>p.aspnet_UserClaims)
-                    .Include(p=>p.aspnet_UserLogin);
+                    .Include(p => p.aspnet_Applications)
+                    .Include(p => p.aspnet_Membership)
+                    .Include(p => p.aspnet_PersonalizationPerUser)
+                    .Include(p => p.aspnet_Profile)
+                    .Include(p => p.aspnet_Roles)
+                    .Include(p => p.aspnet_UserClaims)
+                    .Include(p => p.aspnet_UserLogin);
             }
             catch (Exception ex)
             {
@@ -51,7 +53,14 @@ namespace EdiuxTemplateWebApp.Models.AspNetModels
                 InternalDatabaseAlias.aspnet_Roles.Load();
                 InternalDatabaseAlias.aspnet_UserLogin.Load();
 
-                return ObjectSet.AsQueryable();
+                return InternalDatabaseAlias.aspnet_Users
+                    .Include(p => p.aspnet_Applications)
+                    .Include(p => p.aspnet_Membership)
+                    .Include(p => p.aspnet_PersonalizationPerUser)
+                    .Include(p => p.aspnet_Profile)
+                    .Include(p => p.aspnet_Roles)
+                    .Include(p => p.aspnet_UserClaims)
+                    .Include(p => p.aspnet_UserLogin);
             }
             catch (Exception ex)
             {
@@ -135,25 +144,43 @@ namespace EdiuxTemplateWebApp.Models.AspNetModels
 
         }
 
-        public Task AddAsync(aspnet_Users user)
+        public void AddToRole(string applicationName, string userName, string roleName)
         {
-            try
-            {
-                Add(user.UserName, user.aspnet_Membership.Password, user.aspnet_Applications, user.aspnet_Membership.Email);
-                return Task.CompletedTask;
-            }
-            catch (Exception ex)
-            {
-                WriteErrorLog(ex);
-                return Task.FromException(ex);
-            }
+            InternalDatabaseAlias.aspnet_UsersInRoles_AddUsersToRoles(applicationName, userName, roleName, DateTime.UtcNow);
         }
 
-        public Task<string> GetEmailAsync(aspnet_Users user)
+        public IList<string> FindUsersInRole(string applicationName, string UserNameToMatch, string roleName)
         {
+            return InternalDatabaseAlias.aspnet_UsersInRoles_FindUsersInRole(applicationName, UserNameToMatch, roleName).ToList();
+        }
+
+        public bool IsInRole(string applicationName, string userName, string roleName)
+        {
+
             try
             {
-                return Task.FromResult(user.aspnet_Membership.GetEmail());
+                var applicationNameParameter = applicationName != null ?
+                new SqlParameter("@ApplicationName", applicationName) :
+                new SqlParameter("@ApplicationName", SqlDbType.NVarChar, 256);
+
+                var userNameParameter = userName != null ?
+                    new SqlParameter("@UserName", userName) :
+                    new SqlParameter("@UserName", SqlDbType.NVarChar, 256);
+
+                var roleNameParameter = roleName != null ?
+                    new SqlParameter("@RoleName", roleName) :
+                    new SqlParameter("@RoleName", SqlDbType.NVarChar, 256);
+
+                var returnCode = new SqlParameter();
+                returnCode.ParameterName = "@return_value";
+                returnCode.SqlDbType = SqlDbType.Int;
+                returnCode.Direction = ParameterDirection.Output;
+
+                int code = 0;
+                var result = UnitOfWork.Context.Database.SqlQuery(typeof(int), "EXEC @return_value = [dbo].[aspnet_UsersInRoles_IsUserInRole] @ApplicationName, @UserName, @RoleName", applicationNameParameter, userNameParameter, roleNameParameter, returnCode);
+                // ((IObjectContextAdapter)UnitOfWork.Context).ObjectContext.ExecuteFunction("aspnet_UsersInRoles_IsUserInRole", applicationNameParameter, userNameParameter, roleNameParameter,returnCode);
+                code = (int)returnCode.Value;
+                return (code == 1);
             }
             catch (Exception ex)
             {
@@ -163,43 +190,21 @@ namespace EdiuxTemplateWebApp.Models.AspNetModels
 
         }
 
-        public Task SetEmailAsync(aspnet_Users user, string email)
+        public IList<string> GetRolesForUser(string applicationName, string userName)
         {
+
             try
             {
-                user.aspnet_Membership.SetEmail(email);
-                return Task.CompletedTask;
+                return InternalDatabaseAlias.aspnet_UsersInRoles_GetRolesForUser(applicationName, userName).ToList();
             }
             catch (Exception ex)
             {
                 WriteErrorLog(ex);
                 throw ex;
             }
+
         }
 
-
-
-        public Task AddToRoleAsync(aspnet_Users user, string roleName)
-        {
-            try
-            {
-                if (user.ApplicationId == Guid.Empty)
-                    return Task.FromException(new ArgumentNullException(nameof(user)));
-
-                if (string.IsNullOrEmpty(roleName) || string.IsNullOrWhiteSpace(roleName))
-                    return Task.FromException(new ArgumentNullException(nameof(roleName)));
-
-                string appName = user.aspnet_Applications.ApplicationName;
-                string userName = user.UserName;
-                AddToRole(appName, userName, roleName);
-                return Task.CompletedTask;
-            }
-            catch (Exception ex)
-            {
-                WriteErrorLog(ex);
-                return Task.FromException(ex);
-            }
-        }
         private static void updateCache(aspnet_Applications applicationObject)
         {
             try
@@ -215,300 +220,23 @@ namespace EdiuxTemplateWebApp.Models.AspNetModels
 
 
 
-        public Task DeleteAsync(aspnet_Users user)
-        {
-            try
-            {
-                Delete(user);
-                return Task.CompletedTask;
-            }
-            catch (Exception ex)
-            {
-                WriteErrorLog(ex);
-                return Task.FromException(ex);
-            }
-        }
 
-        public Task<aspnet_Users> FindByIdAsync(Guid userId)
-        {
-            //Remove
-            try
-            {
-                return Task.FromResult(Get(userId));
-            }
-            catch (Exception ex)
-            {
-                WriteErrorLog(ex);
-                return Task.FromException<aspnet_Users>(ex);
-            }
-        }
 
-        public Task<aspnet_Users> FindByNameAsync(string userName)
-        {
-            //Remove
-            try
-            {
-                return Task.FromResult(Where(s => s.UserName == userName).SingleOrDefault());
-            }
-            catch (Exception ex)
-            {
-                WriteErrorLog(ex);
-                return Task.FromException<aspnet_Users>(ex);
-            }
 
-        }
 
-        public Task<IList<string>> GetRolesAsync(aspnet_Users user)
-        {
-            //Remove
-            try
-            {
-                return Task.FromResult((IList<string>)InternalDatabaseAlias.aspnet_UsersInRoles_GetRolesForUser(user.aspnet_Applications.ApplicationName, user.UserName).ToList());
-            }
-            catch (Exception ex)
-            {
-                WriteErrorLog(ex);
-                return Task.FromException<IList<string>>(ex);
-            }
 
-        }
 
-        public Task<bool> IsInRoleAsync(aspnet_Users user, string roleName)
-        {
-            try
-            {
-
-                int returnCode = InternalDatabaseAlias.aspnet_UsersInRoles_IsUserInRole(user.aspnet_Applications.ApplicationName, user.UserName, roleName);
-
-                if (returnCode == 2)
-                    return Task.FromException<bool>(new NullReferenceException("The 'ApplicationId' or 'UserId' is null."));
-
-                if (returnCode == 3)
-                    return Task.FromException<bool>(new NullReferenceException(string.Format("The name of role, {0} is not found.", roleName)));
-
-                if (returnCode == 1)
-                    return Task.FromResult(true);
-                else
-                    return Task.FromResult(false);
-            }
-            catch (Exception ex)
-            {
-                WriteErrorLog(ex);
-                return Task.FromException<bool>(ex);
-            }
-        }
-
-        public Task RemoveFromRoleAsync(aspnet_Users user, string roleName)
-        {
-            try
-            {
-                string appName = user.aspnet_Applications.ApplicationName;
-                var executeResult = InternalDatabaseAlias.aspnet_UsersInRoles_RemoveUsersFromRoles(appName, user.UserName, roleName);
-                return Task.CompletedTask;
-            }
-            catch (Exception ex)
-            {
-                WriteErrorLog(ex);
-                return Task.FromException(ex);
-            }
-        }
-
-        public Task UpdateAsync(aspnet_Users user)
-        {
-            try
-            {
-                aspnet_Users foundUser = Get(user.Id);
-
-                if (foundUser == null)
-                    return Task.FromException(new NullReferenceException(string.Format("The user '{0}' is not found.", user.UserName)));
-
-                foundUser.ApplicationId = user.ApplicationId;
-                foundUser.aspnet_Applications = user.aspnet_Applications;
-                foundUser.aspnet_Membership = user.aspnet_Membership;
-                foundUser.aspnet_PersonalizationPerUser = user.aspnet_PersonalizationPerUser;
-                foundUser.aspnet_Profile = user.aspnet_Profile;
-                foundUser.aspnet_Roles = user.aspnet_Roles;
-                foundUser.aspnet_UserLogin = user.aspnet_UserLogin;
-                foundUser.IsAnonymous = user.IsAnonymous;
-                foundUser.LastActivityDate = user.LastActivityDate;
-                foundUser.LoweredUserName = user.LoweredUserName;
-                foundUser.MobileAlias = user.MobileAlias;
-                foundUser.UserName = user.UserName;
-
-                UnitOfWork.Context.Entry(foundUser).State = System.Data.Entity.EntityState.Modified;
-                UnitOfWork.Commit();
-
-                return Task.CompletedTask;
-            }
-            catch (Exception ex)
-            {
-                WriteErrorLog(ex);
-                return Task.FromException(ex);
-            }
-
-        }
-
-        public Task AddClaimAsync(aspnet_Users user, Claim claim)
-        {
-
-            throw new NotImplementedException();
-        }
-
-        public Task<aspnet_Users> FindByEmailAsync(string email)
-        {
-
-            try
-            {
-                aspnet_Users foundUser = Where(w => w.aspnet_Membership.Email == email).SingleOrDefault();
-                return Task.FromResult(foundUser);
-            }
-            catch (Exception ex)
-            {
-                WriteErrorLog(ex);
-                return Task.FromException<aspnet_Users>(ex);
-            }
-        }
-
-        public Task<int> GetAccessFailedCountAsync(aspnet_Users user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IList<Claim>> GetClaimsAsync(aspnet_Users user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> GetEmailConfirmedAsync(aspnet_Users user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> GetLockoutEnabledAsync(aspnet_Users user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<DateTimeOffset> GetLockoutEndDateAsync(aspnet_Users user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<string> GetPasswordHashAsync(aspnet_Users user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<string> GetPhoneNumberAsync(aspnet_Users user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> GetPhoneNumberConfirmedAsync(aspnet_Users user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<string> GetSecurityStampAsync(aspnet_Users user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> GetTwoFactorEnabledAsync(aspnet_Users user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> HasPasswordAsync(aspnet_Users user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<int> IncrementAccessFailedCountAsync(aspnet_Users user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task RemoveClaimAsync(aspnet_Users user, Claim claim)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task ResetAccessFailedCountAsync(aspnet_Users user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task SetEmailConfirmedAsync(aspnet_Users user, bool confirmed)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task SetLockoutEndDateAsync(aspnet_Users user, DateTimeOffset lockoutEnd)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task SetPasswordHashAsync(aspnet_Users user, string passwordHash)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task SetPhoneNumberAsync(aspnet_Users user, string phoneNumber)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task SetPhoneNumberConfirmedAsync(aspnet_Users user, bool confirmed)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task SetSecurityStampAsync(aspnet_Users user, string stamp)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task SetTwoFactorEnabledAsync(aspnet_Users user, bool enabled)
-        {
-            throw new NotImplementedException();
-        }
     }
 
     public partial interface Iaspnet_UsersRepository : IRepositoryBase<aspnet_Users>
     {
         aspnet_Users Add(string userName, string password, aspnet_Applications applicationObject, string eMail = "");
-
         IQueryable<aspnet_Users> All(aspnet_Applications application);
-        Task AddToRoleAsync(aspnet_Users user, string roleName);
-        Task AddAsync(aspnet_Users user);
-        Task DeleteAsync(aspnet_Users user);
-        //Task<aspnet_Users> FindByIdAsync(Guid userId);
-        //Task<aspnet_Users> FindByNameAsync(string userName);
-        //Task UpdateAsync(aspnet_Users user);
-        //Task<IList<string>> GetRolesAsync(aspnet_Users user);
-        //Task<bool> IsInRoleAsync(aspnet_Users user, string roleName);
-        Task RemoveFromRoleAsync(aspnet_Users user, string roleName);
-        //Task<aspnet_Users> FindByEmailAsync(string email);
-        ////Task<DateTimeOffset> GetLockoutEndDateAsync(aspnet_Users user);
-        //Task SetLockoutEndDateAsync(aspnet_Users user, DateTimeOffset lockoutEnd);
-        //Task<int> IncrementAccessFailedCountAsync(aspnet_Users user);
-        //Task ResetAccessFailedCountAsync(aspnet_Users user);
-        //Task<int> GetAccessFailedCountAsync(aspnet_Users user);
-        //Task<bool> GetLockoutEnabledAsync(aspnet_Users user);
-        //Task SetPasswordHashAsync(aspnet_Users user, string passwordHash);
-        //Task<string> GetPasswordHashAsync(aspnet_Users user);
-        //Task SetPhoneNumberAsync(aspnet_Users user, string phoneNumber);
-        //Task<string> GetPhoneNumberAsync(aspnet_Users user);
-        //Task<bool> HasPasswordAsync(aspnet_Users user);
-        //Task<bool> GetPhoneNumberConfirmedAsync(aspnet_Users user);
-        //Task SetPhoneNumberConfirmedAsync(aspnet_Users user, bool confirmed);
-        //Task SetSecurityStampAsync(aspnet_Users user, string stamp);
-        //Task<string> GetSecurityStampAsync(aspnet_Users user);
-        //Task SetTwoFactorEnabledAsync(aspnet_Users user, bool enabled);
-        //Task<bool> GetTwoFactorEnabledAsync(aspnet_Users user);
-        //Task<IList<Claim>> GetClaimsAsync(aspnet_Users user);
-        Task AddClaimAsync(aspnet_Users user, Claim claim);
-        Task RemoveClaimAsync(aspnet_Users user, Claim claim);
+        void AddToRole(string applicationName, string userName, string roleName);
+        bool IsInRole(string applicationName, string userName, string roleName);
+        IList<string> FindUsersInRole(string applicationName, string UserNameToMatch, string roleName);
+
+        IList<string> GetRolesForUser(string applicationName, string userName);
+
     }
 }

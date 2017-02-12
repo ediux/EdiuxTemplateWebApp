16864690a36a5ceba1258d4da85f3f3633bc2f84
ediux <ediux@msn.com>
2017-02-12 22:19:14 +0800

    加入自訂的分頁式清單類別

---
+++ EdiuxTemplateWebApp/Models/MyPagedList.cs
@@ -0,0 +1,13 @@
+﻿using PagedList;
+using System.Collections.Generic;
+
+namespace EdiuxTemplateWebApp.Models
+{
+    public class MyPagedList<T> : PagedList<T>
+    {
+        public MyPagedList(List<T> source, int PageNumber, int PageSize, int TotalCount) : base(source, PageNumber, PageSize)
+        {
+            TotalItemCount = TotalCount;
+        }
+    }
+}

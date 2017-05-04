f05daf3e3c8d507c66e1beab94f1bd0044b3c481
ediux <ediux@msn.com>
2017-05-04 23:41:28 +0800

    完成Store基底類別的內容

--- EdiuxTemplateWebApp/Models/Shared/StoreBase.cs
+++ EdiuxTemplateWebApp/Models/Shared/StoreBase.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 using System.Data.Entity.Core.Objects;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace EdiuxTemplateWebApp.Models
 {
@@ -17,6 +17,7 @@ namespace EdiuxTemplateWebApp.Models
         public StoreBase()
         {
             repository = GetRepository();
+            Initialization();
         }
 
         protected virtual IRepositoryBase<T> GetRepository()
@@ -24,7 +25,7 @@ namespace EdiuxTemplateWebApp.Models
             Type repoType = typeof(T);
             string repositoryFunctionName = string.Format("Get{0}Repository", repoType.Name);
             Type repoHelper = typeof(RepositoryHelper);
-            var callfunc = repoHelper.GetMethod(repositoryFunctionName, System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+            var callfunc = repoHelper.GetMethod(repositoryFunctionName, BindingFlags.Static | BindingFlags.Public);
             return (IRepositoryBase<T>)callfunc.Invoke(null, new object[] { });
         }
 
@@ -50,7 +51,7 @@ namespace EdiuxTemplateWebApp.Models
         public virtual T CreateDataEntityInstance()
         {
             Type createEntityFuncType = typeof(T);
-            var func = createEntityFuncType.GetMethod("Create", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+            var func = createEntityFuncType.GetMethod("Create", BindingFlags.Static | BindingFlags.Public);
             return (T)func.Invoke(null, new object[] { });
         }
 
@@ -66,7 +67,7 @@ namespace EdiuxTemplateWebApp.Models
             }
         }
 
-        public bool Delete(T entity)
+        public virtual bool Delete(T entity)
         {
             repository.Delete(entity);
             repository.UnitOfWork.Commit();
@@ -92,7 +93,23 @@ namespace EdiuxTemplateWebApp.Models
             return pkeys.ToArray();
         }
 
-        public bool Delete(IEnumerable<T> entities)
+        protected MemberInfo[] IdentifyPrimaryKeyMembers()
+        {
+            ObjectContext objectContext = repository.UnitOfWork.Context.ObjectContext;
+            ObjectSet<T> set = objectContext.CreateObjectSet<T>();
+            IEnumerable<string> keyNames = set.EntitySet.ElementType
+                                                        .KeyMembers
+                                                        .Select(k => k.Name);
+
+            Type entityreflection = typeof(T);
+
+            var pkeys = entityreflection.GetMembers()
+                .Join(keyNames, (x) => x.Name, (y) => y, (k, t) => k);
+
+            return pkeys.ToArray();
+        }
+
+        public virtual bool Delete(IEnumerable<T> entities)
         {
             repository.UnitOfWork.TranscationMode = true;
 
@@ -105,7 +122,7 @@ namespace EdiuxTemplateWebApp.Models
                 }
                 catch
                 {
-                    isbreak = true;                   
+                    isbreak = true;
                     break;
                 }
 
@@ -146,69 +163,127 @@ namespace EdiuxTemplateWebApp.Models
             }
         }
 
-        public IEnumerable<T> FindDataEntitiesByQuery(TKey key)
+        public virtual IEnumerable<T> FindDataEntitiesByQuery(TKey key)
         {
-            throw new NotImplementedException();
+            ParameterExpression numParam = Expression.Parameter(typeof(TKey), "x");
+
+            Expression<Func<T, bool>> wherefiliter = Expression.Lambda<Func<T, bool>>(Expression.Equal(Expression.MakeMemberAccess(numParam, IdentifyPrimaryKeyMembers()
+                .Where(w => w.MemberType == MemberTypes.Property && w.ReflectedType == typeof(TKey)).Single()), Expression.Constant(key)));
+
+            return repository.Where(wherefiliter).ToList();
         }
 
-        public IEnumerable<TResult> FindEntitiesByQuery<TResult>(TKey key, Expression<Func<T, TResult>> selector = null)
+        public virtual IEnumerable<TResult> FindEntitiesByQuery<TResult>(TKey key, Expression<Func<T, TResult>> selector = null)
         {
-            throw new NotImplementedException();
+            ParameterExpression numParam = Expression.Parameter(typeof(TKey), "x");
+
+            Expression<Func<T, bool>> wherefiliter = Expression.Lambda<Func<T, bool>>(Expression.Equal(Expression.MakeMemberAccess(numParam, IdentifyPrimaryKeyMembers()
+                .Where(w => w.MemberType == MemberTypes.Property && w.ReflectedType == typeof(TKey)).Single()), Expression.Constant(key)));
+
+            if (selector != null)
+            {
+                return repository.Where(wherefiliter).Select(selector);
+            }
+
+            return repository.Where(wherefiliter).Select(s => (TResult)Convert.ChangeType(s, typeof(TKey)));
         }
 
         public Task<IEnumerable<TResult>> FindEntitiesByQueryAsync<TResult>(TKey key, Expression<Func<T, TResult>> selector = null)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return Task.FromResult(FindEntitiesByQuery(key, selector));
+            }
+            catch (Exception ex)
+            {
+                return Task.FromException<IEnumerable<TResult>>(ex);
+            }
         }
 
-        public T GetEntityByQuery(TKey key)
+        public virtual T GetEntityByQuery(TKey key)
         {
-            throw new NotImplementedException();
+            return repository.Get(key);
         }
 
         public Task<T> GetEntityByQueryAsync(TKey key)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return Task.FromResult(GetEntityByQuery(key));
+            }
+            catch (Exception ex)
+            {
+                return Task.FromException<T>(ex);
+            }
         }
 
-        public void Initialization()
+        public virtual void Initialization()
         {
-            throw new NotImplementedException();
+            
         }
 
         public Task InitializationAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                Initialization();
+                return Task.CompletedTask;
+            }
+            catch (Exception ex)
+            {
+                return Task.FromException(ex);
+            }
         }
 
-        public bool IsExisted(Expression<Func<T, bool>> filiter)
+        public virtual bool IsExisted(Expression<Func<T, bool>> filiter)
         {
-            throw new NotImplementedException();
+            return repository.Where(filiter).Any();
         }
 
         public Task<bool> IsExistedAsync(Expression<Func<T, bool>> filiter)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return Task.FromResult(IsExisted(filiter));
+            }
+            catch (Exception ex)
+            {
+                return Task.FromException<bool>(ex);
+            }
         }
 
-        public bool Update(T entity)
+        public virtual bool Update(T entity)
         {
             throw new NotImplementedException();
         }
 
-        public bool Update(IEnumerable<T> entities)
+        public virtual bool Update(IEnumerable<T> entities)
         {
             throw new NotImplementedException();
         }
 
         public Task<bool> UpdateAsync(T entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return Task.FromResult(Update(entity));
+            }
+            catch (Exception ex)
+            {
+                return Task.FromException<bool>(ex);
+            }
         }
 
         public Task<bool> UpdateAsync(IEnumerable<T> entities)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return Task.FromResult(Update(entities));
+            }
+            catch (Exception ex)
+            {
+                return Task.FromException<bool>(ex);
+            }
         }
     }
 }
